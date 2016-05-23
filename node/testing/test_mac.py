a54bdeb8af35eca419010f692a05be371e3a1474
Macbook <jesus.cast.sosa@gmail.com>
2016-05-22 20:35:08 -0700

    Put the video source in here because it is small enouhg

--- node/testing/test_mac.py
+++ node/testing/test_mac.py
@@ -12,13 +12,16 @@ if not testing:
 	display = Display(visible = 0, size=(1024, 768))
 	display.start()
 
-video_path = os.getcwd()+"/"+ "carphone_qcif.y4m"
+video_path = os.getcwd()+"/"+ "me_t.y4m"
+audio_path = os.getcwd()+"/"+ "me_s.wav"
+
 initial_url = "http://localhost:3000/"
 
 chrome_options = Options()
 chrome_options.add_argument("--use-fake-device-for-media-stream")
 chrome_options.add_argument("--always-authorize-plugins")
 chrome_options.add_argument("--use-fake-ui-for-media-stream")
+chrome_options.add_argument("--use-file-for-fake-audio-capture="+audio_path)
 chrome_options.add_argument("--use-file-for-fake-video-capture="+video_path)
 
 driver = webdriver.Chrome(chrome_options=chrome_options)
